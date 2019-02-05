b8a98d93722bd9d0af0b5cc25e2661c8628073b4
Cindy Clijsters <cindy.clijsters@gmail.com>
2019-02-05 11:54:23 +0100

    Redirect to overview after deleting a topping & show success/error  message

--- admin/toppingDelete.php
+++ admin/toppingDelete.php
@@ -3,7 +3,7 @@ $root = dirname(__FILE__, 2);
 
 require_once($root . '/vendor/autoload.php');
 
-use App\Business\{AdministratorService, ToppingService};
+use App\Business\{AdministratorService, FlashService, ToppingService};
 
 // Check if the administrator is logged in correctly
 $administratorSvc = new AdministratorService();
@@ -15,10 +15,36 @@ if ($administrator === null) {
 
 // Get the id to the topping
 $toppingId = filter_input(INPUT_GET, 'id', FILTER_VALIDATE_INT);
+$topping   = null;
+
 
 if ($toppingId !== false && $toppingId !== null) {
     $toppingSvc = new ToppingService();
+    $topping    = $toppingSvc->getById($toppingId);
+}
+
+if ($topping !== null) {
+    
     $toppingSvc->delete($toppingId);
+    
+    // Set the delete message
+    $flashSvc = new FlashService();
+    $flashSvc->setFlashMessage(
+        "topping",
+        "De topping is met succes verwijderd.",
+        "warning"
+    );
+    
+} else {
+    
+    // Set the error message
+    $flashSvc = new FlashService();
+    $flashSvc->setFlashMessage(
+        'topping',
+        'Er is een fout opgetreden! De topping is niet verwijderd.',
+        'danger'
+    );    
+    
 }
 
 header("location:topping.php");
