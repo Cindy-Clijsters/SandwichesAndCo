bd4de563fd9c7d217c6e50dda280c8ea07cded65
Cindy Clijsters <cindy.clijsters@gmail.com>
2019-02-05 14:39:27 +0100

    Redirect to overview after deleting a product category & show success/error message

--- admin/productCategoryDelete.php
+++ admin/productCategoryDelete.php
@@ -3,7 +3,7 @@ $root = dirname(__FILE__, 2);
 
 require_once($root . '/vendor/autoload.php');
 
-use App\Business\{AdministratorService, ProductCategoryService};
+use App\Business\{AdministratorService, FlashService, ProductCategoryService};
 
 // Check if the administrator is logged in correctly
 $administratorSvc = new AdministratorService();
@@ -15,10 +15,36 @@ if ($administrator === null) {
 
 // Get the id to the topping
 $productCategoryId = filter_input(INPUT_GET, 'id', FILTER_VALIDATE_INT);
+$productCategory   = null;
 
 if ($productCategoryId !== false && $productCategoryId !== null) {
     $productCategorySvc = new ProductCategoryService();
+    $productCategory    = $productCategorySvc->getById($productCategoryId);
+}
+
+if ($productCategory !== null) {
+    
+    // Delete the link
     $productCategorySvc->delete($productCategoryId);
+    
+    // Set the delete message
+    $flashSvc = new FlashService();
+    $flashSvc->setFlashMessage(
+        'productCategory',
+        'De product categorie is met succes verwijderd.',
+        'warning'
+    );
+    
+} else {
+    
+    // Set the error message
+    $flashSvc = new FlashService();
+    $flashSvc->setFlashMessage(
+        'productCategory',
+        'Er is een fout opgetreden! De product categorie is niet verwijderd.',
+        'danger'
+    );
+    
 }
 
 header("location:productCategory.php");
